f14c2bf89c33f1dc60eff777d77c213675b751ca
NipunEranda <namarasekara71@gmail.com>
2022-08-28 14:43:15 +0530

    navigation ui improved

--- MoneyBro/App.js
+++ MoneyBro/App.js
@@ -1,32 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type { Node } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
+  Image,
+  Text
 } from 'react-native';
-import Login from './src/screens/login';
-import Home from './src/screens/Home';
 
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
+//Screens
+import Home from './src/navigation/screens/Home';
+import Login from './src/navigation/screens/login';
+import Expense from './src/navigation/screens/Expense';
+import Income from './src/navigation/screens/Income';
+import Settings from './src/navigation/screens/Settings';
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -36,12 +34,99 @@ const App: () => Node = () => {
     flex: 1
   };
 
+  function HomeTabs() {
+    return (
+      <Tab.Navigator initialRouteName='Home' screenOptions={{ headerStyle: {backgroundColor: '#242424'}, headerTitleStyle: { color: 'white' } }}>
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require('./src/assets/img/home.png')
+                  : require('./src/assets/img/home.png')
+              }
+              style={{
+                width: size,
+                height: size,
+              }}
+            />
+          ),
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#242424',
+          },
+          tabBarActiveBackgroundColor: '#333333'
+        }} />
+        <Tab.Screen name="Income" component={Income} options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require('./src/assets/img/income.png')
+                  : require('./src/assets/img/income.png')
+              }
+              style={{
+                width: size,
+                height: size,
+              }}
+            />
+          ),
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#242424',
+          },
+          tabBarActiveBackgroundColor: '#333333'
+        }} />
+        <Tab.Screen name="Expense" component={Expense} options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require('./src/assets/img/expense.png')
+                  : require('./src/assets/img/expense.png')
+              }
+              style={{
+                width: size,
+                height: size,
+              }}
+            />
+          ),
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#242424',
+          },
+          tabBarActiveBackgroundColor: '#333333'
+        }} />
+        <Tab.Screen name="Settings" component={Settings} options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require('./src/assets/img/settings.png')
+                  : require('./src/assets/img/settings.png')
+              }
+              style={{
+                width: size,
+                height: size,
+              }}
+            />
+          ),
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#242424',
+          },
+          tabBarActiveBackgroundColor: '#333333'
+        }} />
+      </Tab.Navigator>
+    );
+  }
+
   return (
     <NavigationContainer>
       <SafeAreaView style={backgroundStyle}>
         <Stack.Navigator initialRouteName="Login" screenOptions={{ header: () => null }}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="loginScreen" component={Login} />
+          <Stack.Screen name="homeScreen" component={HomeTabs} />
         </Stack.Navigator>
       </SafeAreaView>
     </NavigationContainer >
@@ -49,22 +134,6 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
 });
 
 export default App;
--- MoneyBro/src/navigation/screens/login.js
+++ MoneyBro/src/navigation/screens/login.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Image, Text, View, StyleSheet, SafeAreaView, BackHandler } from 'react-native';
-import credentials from '../../cred.json';
-import app from '../../app.json';
+import credentials from '../../../cred.json';
+import app from '../../../app.json';
 import axios from 'axios';
-const logo = require('../assets/img/app.png');
+const logo = require('../../assets/img/app.png');
 import {
     GoogleSignin,
     GoogleSigninButton,
     statusCodes,
 } from '@react-native-google-signin/google-signin';
+
 
 export default class Login extends Component {
 
@@ -16,7 +17,7 @@ export default class Login extends Component {
         GoogleSignin.configure({
             ClientId: credentials.web.client_id,
         });
-        BackHandler.addEventListener('hardwareBackPress', function() {return true})
+        BackHandler.addEventListener('hardwareBackPress', function () { return true })
 
     }
 
@@ -27,7 +28,7 @@ export default class Login extends Component {
             const user = { sub: userInfo.user.id, name: userInfo.user.name, email: userInfo.user.email, balance: 0, currency: 'USD' };
             const response = await axios.post(`${app.api}/.netlify/functions/auth/google`, user);
             if (response.data) {
-                this.props.navigation.push("Home");
+                this.props.navigation.push("homeScreen");
             }
 
         } catch (error) {
@@ -69,6 +70,6 @@ const styles = StyleSheet.create({
     login_icon: {
         width: 200,
         height: 200,
-        marginTop: 150
+        marginTop: 150,
     },
 });
